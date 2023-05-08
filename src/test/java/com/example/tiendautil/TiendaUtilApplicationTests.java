18546e6c2b60bbc111baea146c55b5902d0a7505
Rahmat Velasquez <rahmatve9@gmail.com>
2023-05-08 01:16:25 -0500

    Estructura inicial de proyecto

---
+++ src/test/java/com/example/tiendautil/TiendaUtilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tiendautil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaUtilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
